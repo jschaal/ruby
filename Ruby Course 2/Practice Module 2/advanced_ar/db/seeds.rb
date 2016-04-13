52dc7f4039012189c0ca56a65a7f23b852d60b09
Joe Schaal <joe@schaal.net>
2016-04-13 16:14:39 -0400

    practices changes from laptop

--- Ruby Course 2/Practice Module 2/advanced_ar/db/seeds.rb
+++ Ruby Course 2/Practice Module 2/advanced_ar/db/seeds.rb
@@ -16,3 +16,13 @@ Person.create! [
   { first_name: "Bill", last_name: "Gates", age: 75, login: "bill", pass: "windows3.1" },
   { first_name: "LeBron", last_name: "James", age: 30, login: "bron", pass: "need more rings" }
 ]
+
+Person.first.jobs.create! [
+  { title: "Developer", company: "MS", position_id: "#1234" },
+  { title: "Developer", company: "MS", position_id: "#1235" }
+]
+
+Person.last.jobs.create! [
+  { title: "Sr. Developer", company: "MS", position_id: "#5234" },
+  { title: "Sr. Developer", company: "MS", position_id: "#5235" }
+]
